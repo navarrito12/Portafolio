061564caec15820f8b064a2ab4ac51bf8e77e17b
navarrito12 <andreanavarroojopi@gmail.com>
2025-11-16 15:57:05 +0100

    24/10/2025

---
+++ Bluej/Ambito y tiempo de vida de las variables/Principal.java
@@ -0,0 +1,8 @@
+public class Principal
+{
+    public static void main(String[] args) {
+        Robot r1=new Robot();
+        // r1.numero=3;         <- Error
+        // r1.contador=4;       <- Error
+    }
+}
