cd1f18e42156faea4c26b63bd07185e3d795901e
navarrito12 <andreanavarroojopi@gmail.com>
2025-11-16 15:39:15 +0100

    29/09/2025

---
+++ Bluej/Alumnos/Principal.java
@@ -0,0 +1,10 @@
+public class Principal {
+    public static void main(String[] args) {
+        Alumno a1=new Alumno("ALONSO","Alonso","Perez","Garcia",2005);
+        Alumno a2=new Alumno("PERICA","Perica","Lopez","Soto",1987);
+        Alumno a3=new Alumno("ANA","Ana","Gomez","Ruiz",1985);
+        System.out.println(a1);
+        System.out.println(a2);
+        System.out.println(a3);
+    }
+}
