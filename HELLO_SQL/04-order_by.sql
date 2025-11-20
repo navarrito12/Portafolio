9440948e28288b6ce1afad56143d1540d48d49ca
navarrito12 <andreanavarroojopi@gmail.com>
2025-11-20 19:29:49 +0100

    Delete HELLO_SQL/04-order_by.sql

--- HELLO_SQL/04-order_by.sql
+++
@@ -1,9 +0,0 @@
-SELECT * FROM users ORDER BY age;
-
-SELECT * FROM users ORDER BY age ASC;
-
-SELECT * FROM users ORDER BY age DESC;
-
-SELECT * FROM users WHERE email='pedro@gmail.com' ORDER BY age ASC;
-
-SELECT name FROM users WHERE email='pedro@gmail.com' ORDER BY age ASC;
