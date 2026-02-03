09591a4b12456902ad406832035645ebb5910e5c
Andrea Navarro <andreanavarroojopi@gmail.com>
2026-02-03 14:07:05 +0100

    Java

---
+++ Intellij/Urnas y bolas 0p4/src/util/Util.java
@@ -0,0 +1,13 @@
+package util;
+
+public class Util
+{
+    private static final String LINEA="-".repeat(50);
+    
+    private Util() {}
+   
+    // Mostrar línea
+    public static void linea() {    
+        System.out.println(LINEA); 
+    }   
+}
